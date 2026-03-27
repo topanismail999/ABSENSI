28ea484de7c303d64536010ac082c2edba1bf085
topanismail999 <topanismail999@gmail.com>
2026-03-27 15:26:42 +0700

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iisexzdhptmhpzwpfenz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlpc2V4emRocHRtaHB6d3BmZW56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MDU5NTcsImV4cCI6MjA4ODA4MTk1N30.WQPQ5GU9ddcg5TWLWBgjLebW0sXoWPjOfo-cgRfgSTU';
+const supabaseUrl = 'https://atenydidkknrojmmksdo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0ZW55ZGlka2tucm9qbW1rc2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NTI3MjUsImV4cCI6MjA4ODAyODcyNX0.mU_YCS4yt2sqXA_1SCCNQUmk0TlSHKd-gEuGn5Z6TSI';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
