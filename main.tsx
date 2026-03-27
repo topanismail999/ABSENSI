fff281c5300cbd63555ae60c801354247b38a030
topanismail999 <topanismail999@gmail.com>
2026-03-27 15:37:32 +0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Admin from './Admin';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AttendanceKiosk from './App'
+import AdminDashboard from './Admin'
+import './index.css' // Pastikan file css tailwind ada
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/backoffice-secret" element={<Admin />} />
+        {/* Halaman utama untuk Karyawan Absen */}
+        <Route path="/" element={<AttendanceKiosk />} />
+        
+        {/* Halaman rahasia untuk Admin */}
+        <Route path="/admin" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
